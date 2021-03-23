75437868d859415391561b8e4542405a7cb6bcff
Mees van Loben Sels <mavanlobensels@gmail.com>
2021-03-23 12:26:09 +0100

    test.py edit

--- test.py
+++ test.py
@@ -28,9 +28,15 @@ if __name__ == "__main__":
 
     opt = TestOptions().parse()
     model = create_model(opt)
-    model.netEN.module.load_state_dict(torch.load("EN.pkl"))
-    model.netDE.module.load_state_dict(torch.load("DE.pkl"))
-    model.netMEDFE.module.load_state_dict(torch.load("MEDEF.pkl"))
+
+    # model.netEN.module.load_state_dict(torch.load("EN.pkl"))
+    # model.netDE.module.load_state_dict(torch.load("DE.pkl"))
+    # model.netMEDFE.module.load_state_dict(torch.load("MEDEF.pkl"))
+    param_paths = sorted(glob('./checkpoints/Mutual-Encoder-Decoder_10000_images_2_epochs/*.pth'), reverse = True)
+    print(param_paths[2])
+    print(param_paths[3])
+    print(param_paths[0])
+
     results_dir = r'./result'
     if not os.path.exists( results_dir):
         os.mkdir(results_dir)
